47f1d9eb3c35938b3754845cc8589d920be9fbce
Carolina Teixeira <carolina.txrs@gmail.com>
2022-12-18 22:38:00 -0400

    Cors instalado e configurado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = require('./router');
+var cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 //SINALIZANDO JSON E AS ROTAS
 app.use(express.json());
+
+app.use('/files', express.static('upload'));
+
+app.use((request, response, next) => {
+  var cors = require('cors');
+  response.header('Access-Control-Allow-Origin', '*');
+  app.use(cors());
+  next();
+});
+
 app.use(router);
 
 module.exports = app;
